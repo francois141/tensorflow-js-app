628aa34f43bb7a6450547771b557c1819c99a4e7
francois141 <frankost.costa@gmail.com>
2022-11-05 22:14:23 +0100

    feature: adding train button + improve canvas

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
 import { MnistData } from './data.js';
 
-async function showExamples(data) {
-    // Create a container in the visor
-    const surface =
-        tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data' });
+/** Some global variables we need for the program **/
+var model = null;
 
-    // Get the examples
-    const examples = data.nextTestBatch(20);
-    const numExamples = examples.xs.shape[0];
+const canvas = document.getElementById("drawCanvas");
+const ctx = canvas.getContext("2d");
+var coord = { x: 0, y: 0 };
 
-    // Create a canvas element to render each example
-    for (let i = 0; i < numExamples; i++) {
-        const imageTensor = tf.tidy(() => {
-            // Reshape the image to 28x28 px
-            return examples.xs
-                .slice([i, 0], [1, examples.xs.shape[1]])
-                .reshape([28, 28, 1]);
-        });
+const classNames = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-        const canvas = document.createElement('canvas');
-        canvas.width = 28;
-        canvas.height = 28;
-        canvas.style = 'margin: 4px;';
-        await tf.browser.toPixels(imageTensor, canvas);
-        surface.drawArea.appendChild(canvas);
+startProgram();
 
-        imageTensor.dispose();
-    }
-}
+function startProgram() {
+    fillRectWithBlack();
 
-function getModel() {
+    document.addEventListener('mousedown', startDraw);
+    document.addEventListener('mouseup', stopDraw);
+} 
+
+/** These 3 functions are for the creation, training and inference part of the deep learning model **/
+
+function createModel() {
 
     const model = tf.sequential();
 
     const INPUT_SHAPE = [28, 28, 1];
     const NUM_OUTPUT_CLASSES = 10;
 
-    // Step 1) Add some layers
     model.add(tf.layers.conv2d({
         inputShape: INPUT_SHAPE,
         kernelSize: 5,
@@ -45,6 +35,7 @@ function getModel() {
         activation: 'relu'
     }));
     model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
+
     model.add(tf.layers.flatten());
     model.add(tf.layers.dense({
         units: NUM_OUTPUT_CLASSES,
@@ -62,7 +53,7 @@ function getModel() {
     return model;
 }
 
-async function train(model, data) {
+async function trainModel(model, data) {
     const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
     const container = {
         name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
@@ -100,26 +91,7 @@ async function train(model, data) {
     });
 }
 
-async function run() {
-
-    const data = new MnistData();
-    await data.load();
-    await showExamples(data);
-
-    const model = getModel();
-    tfvis.show.modelSummary({ name: 'Model Architecture', tab: 'Model' }, model);
-
-    await train(model, data);
-
-    await showAccuracy(model, data);
-    await showConfusion(model, data);
-
-    return model;
-}
-
-const classNames = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-function doPrediction(model, data, testDataSize = 500) {
+function inferModel(model, data, testDataSize = 500) {
 
     const testData = data.nextTestBatch(testDataSize);
     const textxs = testData.xs.reshape([testDataSize, 28, 28, 1]);
@@ -130,8 +102,39 @@ function doPrediction(model, data, testDataSize = 500) {
     return [preds, labels];
 }
 
+/** These 3 functions are for visualisation purposes **/
+
+async function showExamples(data) {
+    // Create a container in the visor
+    const surface =
+        tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data' });
+
+    // Get the examples
+    const examples = data.nextTestBatch(20);
+    const numExamples = examples.xs.shape[0];
+
+    // Create a canvas element to render each example
+    for (let i = 0; i < numExamples; i++) {
+        const imageTensor = tf.tidy(() => {
+            // Reshape the image to 28x28 px
+            return examples.xs
+                .slice([i, 0], [1, examples.xs.shape[1]])
+                .reshape([28, 28, 1]);
+        });
+
+        const canvas = document.createElement('canvas');
+        canvas.width = 28;
+        canvas.height = 28;
+        canvas.style = 'margin: 4px;';
+        await tf.browser.toPixels(imageTensor, canvas);
+        surface.drawArea.appendChild(canvas);
+
+        imageTensor.dispose();
+    }
+}
+
 async function showAccuracy(model, data) {
-    const [preds, labels] = doPrediction(model, data);
+    const [preds, labels] = inferModel(model, data);
     const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
     const container = { name: 'Accuracy', tab: 'Evaluation' };
     tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
@@ -140,62 +143,86 @@ async function showAccuracy(model, data) {
 }
 
 async function showConfusion(model, data) {
-    const [preds, labels] = doPrediction(model, data);
+    const [preds, labels] = inferModel(model, data);
     const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
     const container = { name: 'Confusion Matrix', tab: 'Evaluation' };
     tfvis.render.confusionMatrix(container, { values: confusionMatrix, tickLabels: classNames });
 }
 
-var canvas = document.getElementById("drawCanvas");
-
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.lineWidth = "6";
-ctx.strokeStyle = "#000000";
-ctx.rect(0, 0, 560, 560);
-ctx.stroke();
-
-let coord = { x: 0, y: 0 };
-
+/** These 5 functions are for the canvas **/
 
 function startDraw(event) {
-    document.addEventListener('mousemove', draw);
-    reposition(event);
-}
-function reposition(event) {
-    coord.x = event.clientX - canvas.offsetLeft;
-    coord.y = event.clientY - canvas.offsetTop;
+    document.addEventListener('mousemove', updateDraw);
+    repositionDraw(event);
 }
 
-function draw(event) {
+function updateDraw(event) {
     ctx.beginPath();
     ctx.lineWidth = 20;
     ctx.lineCap = 'round';
-    ctx.strokeStyle = "#000000";
+    ctx.strokeStyle = "#FFFFFF";
     ctx.moveTo(coord.x, coord.y);
-    reposition(event);
+    repositionDraw(event);
     ctx.lineTo(coord.x, coord.y);
     ctx.stroke();
 }
 
-function stopDraw() {
-    document.removeEventListener('mousemove', draw);
+function repositionDraw(event) {
+    coord.x = event.clientX - canvas.offsetLeft;
+    coord.y = event.clientY - canvas.offsetTop;
 }
 
-document.addEventListener('mousedown', startDraw);
-document.addEventListener('mouseup', stopDraw);
+function stopDraw() {
+    document.removeEventListener('mousemove', updateDraw);
+}
 
+function fillRectWithBlack() {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, 560, 560);
+}
+
+/** These 3 functions can be used by the buttons in the front end **/
+
+export async function train() {
+
+    const data = new MnistData();
+
+    await data.load();
+    await showExamples(data);
+
+    const created_model = createModel();
+    tfvis.show.modelSummary({ name: 'Model Architecture', tab: 'Model' }, created_model);
+
+    await trainModel(created_model, data);
+    await showAccuracy(created_model, data);
+    await showConfusion(created_model, data);
+
+    model = created_model;
+}
 
 export async function inference() {
+
     var image = ctx.getImageData(0, 0, canvas.height, canvas.width);
+
     image = tf.browser.fromPixels(image);
     image = tf.image.resizeBilinear(image, [28, 28])
     image = tf.mean(image, 2, true)
     image = image.reshape([1, 28, 28, 1]);
-    console.log("Loading the model")
-    let model = await run();
-    model.predict(image).argMax(-1).data().then(data => {
+
+    if (model == null) {
         let placeholder = document.getElementById("prediction");
-        placeholder.innerText = data[0];
-    });
+        placeholder.innerText = "Model is not trained";
+        return;
+    }
+    else {
+        model.predict(image).argMax(-1).data().then(data => {
+            let placeholder = document.getElementById("prediction");
+            placeholder.innerText = data[0];
+        });
+    }
+
+}
+
+export function clear() {
+    fillRectWithBlack();
 }
